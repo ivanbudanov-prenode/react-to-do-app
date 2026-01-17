ba2571a620bbde6f1f7e7b4eb6eea6efb46ef4b8
ivanbudanov-prenode <ivan.budanov@prenode.de>
2026-01-17 19:52:24 +0100

    the CORS issue that appeared once I added mongoDB is now gone somehow, backend is fetching data from db correctly, but it is not being displayed in frontend for some reason

--- server/server.js
+++ server/server.js
@@ -5,10 +5,43 @@ import {MongoClient, ServerApiVersion} from 'mongodb';
 
 const app = express();
 
+
 app.use(express.json());
 app.use(cors());
+/*app.use(cors({
+    origin: 'http://localhost:5173',
+}))*/
+/*app.use(cors({
+    origin: true,
+}))*/
+/*app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});*/
+/*var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.configure(function() {
+    app.use(allowCrossDomain);
+    //some other code
+});  */  
+
 
 let tasks = [{name: 'John', id: 1, isEditing: true, isChecked: false}, {name: 'Martha', id: 2, isEditing: true, isChecked: false}, {name: 'Luke', id: 3, isEditing: true, isChecked: false}];
+
+const uri = "mongodb+srv://Ivan:password123password123@cluster0.8uzkrrx.mongodb.net/?appName=Cluster0";
+  const client = new MongoClient(uri);
+const database = client.db('test_db');
+    const myColl = database.collection("tasks");
+    const doc = [{name: 'John', id: 1, isEditing: true, isChecked: false}, {name: 'Martha', id: 2, isEditing: true, isChecked: false}, {name: 'Luke', id: 3, isEditing: true, isChecked: false}];
+    const result = await myColl.insertMany(doc);
+    console.log(
+      `A document was inserted with the _id: ${result.insertedId}`,
+    );
 
 async function runGetStarted() {
   const uri = "mongodb+srv://Ivan:password123password123@cluster0.8uzkrrx.mongodb.net/?appName=Cluster0";
@@ -34,14 +67,27 @@ async function runGetStarted() {
     await client.close();
   }
 }
-runGetStarted().catch(console.dir);
+/*runGetStarted().catch(console.dir);*/
 
+async function getTasks() {
+  const result = await database.collection("tasks").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    return result;
+  });
+  return result;
+}
 
 /* endpoint for getting all tasks */
-app.get('/tasks', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({
+app.get('/tasks', async (req, res) => {
+  /*res.setHeader('Access-Control-Allow-Origin', '*');*/
+  /*res.json({
     tasks
+  });*/
+  const data = await getTasks();
+  console.log(data);
+  res.json({
+    data
   });
 });
 
@@ -50,7 +96,7 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 /* endpoint for adding a task */
 app.post('/tasks', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  /*res.setHeader('Access-Control-Allow-Origin', '*');*/
   console.log(req.body);
   tasks.push(req.body);
 
@@ -63,7 +109,7 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 /* endpoint for updating a task */
 app.put('/tasks', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  /*res.setHeader('Access-Control-Allow-Origin', '*');*/
   console.log(req.body);
   let result = tasks.find(obj => obj.id === req.body.id);
   const taskIndex = tasks.findIndex(x => x.id === req.body.id);
